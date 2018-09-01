775485f1d524ab3da08cf79441350d53bbab17fa
Monisha Bhatia <monishabhatia@Monishas-MacBook-Air.local>
2018-09-01 21:03:19 +0530

    bottom sheet

--- app/src/main/java/pros/app/com/pros/detail/fragment/DetailFragment.java
+++ app/src/main/java/pros/app/com/pros/detail/fragment/DetailFragment.java
@@ -5,16 +5,22 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.squareup.picasso.Picasso;
@@ -27,12 +33,16 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 import pros.app.com.pros.R;
+import pros.app.com.pros.base.CustomDialogFragment;
+import pros.app.com.pros.base.CustomDialogListener;
 import pros.app.com.pros.base.DateUtils;
+import pros.app.com.pros.detail.adapter.CommentsAdapter;
 import pros.app.com.pros.detail.adapter.MentionsAdapter;
 import pros.app.com.pros.detail.adapter.ReactionAthlete;
+import pros.app.com.pros.detail.presenter.DetailPresenter;
+import pros.app.com.pros.detail.view.DetailView;
 import pros.app.com.pros.home.model.AthleteModel;
 import pros.app.com.pros.home.model.PostModel;
-import pros.app.com.pros.search.adapter.TopProsAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -42,7 +52,7 @@ import pros.app.com.pros.search.adapter.TopProsAdapter;
  * Use the {@link DetailFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DetailFragment extends Fragment  {
+public class DetailFragment extends Fragment implements DetailView, CustomDialogListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_POST_MODEL = "post_model";
@@ -54,51 +64,58 @@ public class DetailFragment extends Fragment  {
 
     @BindView(R.id.athlete_name)
     TextView athleteName;
-
     @BindView((R.id.created_at))
     TextView createdAt;
-
     @BindView(R.id.thumbnail_background)
     ImageView thumbnailBackground;
-
     @BindView(R.id.athlete_thumb)
     CircleImageView athleteThumb;
-
     @BindView(R.id.likes_count)
     TextView likesCount;
-
     @BindView(R.id.comment_count)
     TextView commentsCount;
-
     @BindView(R.id.comments_iv)
     ImageView commentsIcon;
-
     @BindView(R.id.question_container)
     ConstraintLayout questionContainer;
-
     @BindView(R.id.question_text)
     TextView questionText;
-
     @BindView(R.id.question_athlete_name)
     TextView questionAthleteName;
-
     @BindView(R.id.videoView)
     VideoView videoView;
-
     @BindView(R.id.athlete_list)
     RecyclerView athleteRecyclerview;
-
     @BindView(R.id.mentions_list)
     RecyclerView mentionsListRecyclerview;
-
     @BindView(R.id.tags_iv)
     ImageView mentionsIcon;
+
+    @BindView(R.id.bsButtons)
+    View bsButtons;
+    @BindView(R.id.btn1)
+    Button btn1;
+    @BindView(R.id.btn2)
+    Button btn2;
+
+    @BindView(R.id.bsComments)
+    View bsComments;
+    @BindView(R.id.edtComments)
+    EditText edtComments;
+    @BindView(R.id.tvNumOfComments)
+    TextView tvNumOfComments;
+    @BindView(R.id.rv_comments)
+    RecyclerView rvComments;
+
+    private BottomSheetBehavior behavior;
 
     boolean videoVisibleToUser = false;
 
     private int reactionVideoIndex = 0;
     private ArrayList<String> totalReactionsVideos = new ArrayList<>();
     private boolean toggleMentions = false;
+    private DetailPresenter detailPresenter;
+    private CommentsAdapter adapter;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -126,6 +143,8 @@ public class DetailFragment extends Fragment  {
         if (getArguments() != null) {
             receivedPostModel = getArguments().getParcelable(ARG_POST_MODEL);
         }
+
+        detailPresenter = new DetailPresenter(this);
     }
 
     @Override
@@ -139,22 +158,34 @@ public class DetailFragment extends Fragment  {
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+
+        rvComments.setLayoutManager(layoutManager);
+        adapter = new CommentsAdapter(getContext(), receivedPostModel.getComments());
+        rvComments.setAdapter(adapter);
+
+    }
+
     private void setupUI() {
         String contentType = receivedPostModel.getContentType();
         String athleteFullName = "";
         String thumbnailUrl = "";
         String athleteThumbnailUrl = "";
 
-        if(contentType != null &&
+        if (contentType != null &&
                 (contentType.equalsIgnoreCase("image") || contentType.equalsIgnoreCase("video"))) {
-            thumbnailUrl= receivedPostModel.getUrls().getThumbnailUrl();
-            athleteThumbnailUrl =  receivedPostModel.getAthlete().getAvatar().getThumbnailUrl();
+            thumbnailUrl = receivedPostModel.getUrls().getThumbnailUrl();
+            athleteThumbnailUrl = receivedPostModel.getAthlete().getAvatar().getThumbnailUrl();
             athleteFullName = receivedPostModel.getAthlete().getFirstName() + " " + receivedPostModel.getAthlete().getLastName();
             commentsCount.setVisibility(View.VISIBLE);
             commentsIcon.setVisibility(View.VISIBLE);
-            commentsCount.setText(""+receivedPostModel.getComments().size());
+            commentsCount.setText("" + receivedPostModel.getComments().size());
 
-        } else if(receivedPostModel.getQuestioner() != null){
+        } else if (receivedPostModel.getQuestioner() != null) {
             athleteFullName = receivedPostModel.getQuestioner().getName();
             thumbnailUrl = receivedPostModel.getQuestioner().getAvatar().getThumbnailUrl();
             athleteThumbnailUrl = receivedPostModel.getQuestioner().getAvatar().getThumbnailUrl();
@@ -163,12 +194,12 @@ public class DetailFragment extends Fragment  {
             questionAthleteName.setText(receivedPostModel.getQuestioner().getName());
 
             List<PostModel> reactionsList = receivedPostModel.getReactions();
-            if(reactionsList.size() >0){
-                for(int i=0; i< reactionsList.size(); i++) {
+            if (reactionsList.size() > 0) {
+                for (int i = 0; i < reactionsList.size(); i++) {
                     totalReactionsVideos.add(reactionsList.get(i).getUrls().getMobileUrl());
                 }
 
-               playAllVideos();
+                playAllVideos();
 
 
             }
@@ -181,16 +212,21 @@ public class DetailFragment extends Fragment  {
         Picasso.get().load(thumbnailUrl).into(thumbnailBackground);
         Picasso.get().load(athleteThumbnailUrl).into(athleteThumb);
         createdAt.setText(dateDifference);
-        likesCount.setText("" + receivedPostModel.getLikes().getCount());
+        likesCount.setText(String.valueOf(receivedPostModel.getLikes().getCount()));
+        if(receivedPostModel.getLikes().isLikedByCurrentUser()){
+            likesCount.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_liked, 0, 0 , 0);
+        } else{
+            likesCount.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_unlike, 0, 0 , 0);
+        }
 
         List<PostModel> reactionsList = receivedPostModel.getReactions();
         List<AthleteModel> mentionsList = receivedPostModel.getMentions();
 
-        if(reactionsList.size() >0){
+        if (reactionsList.size() > 0) {
 
             ArrayList<AthleteModel> athleteModels = new ArrayList<>();
 
-            for(int i=0; i<reactionsList.size(); i++){
+            for (int i = 0; i < reactionsList.size(); i++) {
                 athleteModels.add(reactionsList.get(i).getAthlete());
             }
 
@@ -201,7 +237,7 @@ public class DetailFragment extends Fragment  {
         }
 
 
-        if(mentionsList.size() >0){
+        if (mentionsList.size() > 0) {
             mentionsIcon.setVisibility(View.VISIBLE);
             MentionsAdapter mentionsAdapter = new MentionsAdapter(mentionsList);
 
@@ -209,9 +245,9 @@ public class DetailFragment extends Fragment  {
             mentionsListRecyclerview.setAdapter(mentionsAdapter);
         }
 
-        if(contentType != null && contentType.equalsIgnoreCase("video")){
+        if (contentType != null && contentType.equalsIgnoreCase("video")) {
             videoView.setVideoPath(receivedPostModel.getUrls().getMobileUrl());
-            if(videoVisibleToUser){
+            if (videoVisibleToUser) {
                 videoView.start();
             }
             videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
@@ -225,10 +261,10 @@ public class DetailFragment extends Fragment  {
     }
 
     private void playAllVideos() {
-        if(reactionVideoIndex < totalReactionsVideos.size()) {
+        if (reactionVideoIndex < totalReactionsVideos.size()) {
             videoView.setVideoPath(totalReactionsVideos.get(reactionVideoIndex));
             reactionVideoIndex++;
-            if(videoVisibleToUser){
+            if (videoVisibleToUser) {
                 videoView.start();
             }
             videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
@@ -238,15 +274,15 @@ public class DetailFragment extends Fragment  {
                     questionContainer.setVisibility(View.GONE);
                 }
             });
-                videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mp) {
-                        videoView.stopPlayback();
-                        videoView.suspend();
-                        playAllVideos();
-                    }
-                });
-            }
+            videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    videoView.stopPlayback();
+                    videoView.suspend();
+                    playAllVideos();
+                }
+            });
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -282,59 +318,58 @@ public class DetailFragment extends Fragment  {
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
 
-            if (!isVisibleToUser)   // If we are becoming invisible, then...
-            {
-                //pause or stop vide
-                if(videoView != null) {
-                    videoView.stopPlayback();
-                }
-                videoVisibleToUser = false;
+        if (!isVisibleToUser)   // If we are becoming invisible, then...
+        {
+            //pause or stop vide
+            if (videoView != null) {
+                videoView.stopPlayback();
             }
+            videoVisibleToUser = false;
+        }
 
-            if (isVisibleToUser) // If we are becoming visible, then...
-            {
-                videoVisibleToUser = true;
-                if(receivedPostModel !=null) {
-                    String contentType = receivedPostModel.getContentType();
+        if (isVisibleToUser) // If we are becoming visible, then...
+        {
+            videoVisibleToUser = true;
+            if (receivedPostModel != null) {
+                String contentType = receivedPostModel.getContentType();
 
-                    //play your video
-                    if (contentType != null && contentType.equalsIgnoreCase("video") && videoView != null) {
-                        videoView.setVideoPath(receivedPostModel.getUrls().getMobileUrl());
-                        videoView.start();
-                    }
-                    else if(receivedPostModel.getQuestioner() != null){
-                        List<PostModel> reactionsList = receivedPostModel.getReactions();
-                        if(reactionsList.size() >0){
-                            for(int i=0; i< reactionsList.size(); i++) {
-                                totalReactionsVideos.add(reactionsList.get(i).getUrls().getMobileUrl());
-                            }
-
-                            playAllVideos();
-
+                //play your video
+                if (contentType != null && contentType.equalsIgnoreCase("video") && videoView != null) {
+                    videoView.setVideoPath(receivedPostModel.getUrls().getMobileUrl());
+                    videoView.start();
+                } else if (receivedPostModel.getQuestioner() != null) {
+                    List<PostModel> reactionsList = receivedPostModel.getReactions();
+                    if (reactionsList.size() > 0) {
+                        for (int i = 0; i < reactionsList.size(); i++) {
+                            totalReactionsVideos.add(reactionsList.get(i).getUrls().getMobileUrl());
                         }
 
+                        playAllVideos();
+
                     }
 
                 }
 
             }
+
+        }
     }
 
     @OnClick(R.id.tags_iv)
-    void onMentionIconClick(){
+    void onMentionIconClick() {
         toggleMentions = !toggleMentions;
-        if(toggleMentions) {
+        if (toggleMentions) {
             mentionsListRecyclerview.setVisibility(View.VISIBLE);
-        }else {
+        } else {
             mentionsListRecyclerview.setVisibility(View.GONE);
         }
     }
 
     @OnClick(R.id.share_iv)
-    void shareDetails(){
-        if(receivedPostModel != null) {
+    void shareDetails() {
+        if (receivedPostModel != null) {
 
-            String shareText = receivedPostModel.getShareText()+ " " + receivedPostModel.getShareUrl();
+            String shareText = receivedPostModel.getShareText() + " " + receivedPostModel.getShareUrl();
 
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
@@ -342,6 +377,27 @@ public class DetailFragment extends Fragment  {
             sendIntent.setType("text/plain");
             getActivity().startActivity(Intent.createChooser(sendIntent, "Send To"));
         }
+    }
+
+    @Override
+    public void onLikeSuccess() {
+        likesCount.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_liked, 0, 0 , 0);
+        likesCount.setText(String.valueOf(receivedPostModel.getLikes().getCount() + 1));
+        receivedPostModel.getLikes().setLikedByCurrentUser(true);
+        receivedPostModel.getLikes().setCount(receivedPostModel.getLikes().getCount() + 1);
+    }
+
+    @Override
+    public void onUnLikeSuccess() {
+        likesCount.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_unlike, 0, 0 , 0);
+        likesCount.setText(String.valueOf(receivedPostModel.getLikes().getCount() - 1));
+        receivedPostModel.getLikes().setLikedByCurrentUser(false);
+        receivedPostModel.getLikes().setCount(receivedPostModel.getLikes().getCount() - 1);
+    }
+
+    @Override
+    public void onFailure(int message) {
+        Toast.makeText(getContext(), getString(message), Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -358,4 +414,128 @@ public class DetailFragment extends Fragment  {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    @OnClick(R.id.likes_count)
+    public void onclickLike() {
+
+        if(TextUtils.isEmpty(receivedPostModel.getContentType())) {
+            if(receivedPostModel.getLikes().isLikedByCurrentUser()) {
+                detailPresenter.unlikeQuestion(receivedPostModel.getId());
+            } else {
+                detailPresenter.likeQuestion(receivedPostModel.getId());
+            }
+        } else{
+
+            if(receivedPostModel.getLikes().isLikedByCurrentUser()) {
+                detailPresenter.unlikePost(receivedPostModel.getId());
+            } else {
+                detailPresenter.likePost(receivedPostModel.getId());
+            }
+        }
+    }
+
+    @OnClick(R.id.report_iv)
+    public void onclickReport(){
+
+        btn1.setText("Report");
+
+        behavior = BottomSheetBehavior.from(bsButtons);
+        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        break;
+
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+    }
+
+    @OnClick(R.id.btn1)
+    public void onClickbtn1() {
+        behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        confirmationDialog();
+    }
+
+    @OnClick(R.id.btn2)
+    public void onClickbtn2() {
+        behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+    }
+
+    private void confirmationDialog() {
+        CustomDialogFragment customDialogFragment = new CustomDialogFragment();
+        customDialogFragment.registerCallbackListener(this);
+        Bundle bundle = new Bundle();
+        bundle.putString("Title", getString(R.string.report_title));
+        bundle.putString("Content", getString(R.string.report_content));
+        bundle.putString("Action1", "Report");
+        bundle.putString("Action2", "Cancel");
+        customDialogFragment.setArguments(bundle);
+        customDialogFragment.show(getActivity().getSupportFragmentManager(), CustomDialogFragment.TAG);
+    }
+
+    @Override
+    public void handleYes() {
+        detailPresenter.flagPost(receivedPostModel.getId());
+    }
+
+    @Override
+    public void handleNo() {
+
+    }
+
+    @Override
+    public void onflagPostSuccess() {
+        Bundle bundle = new Bundle();
+        bundle.putString("Title", "Success");
+        bundle.putString("Content", getString(R.string.reported_content));
+        bundle.putString("Action2", "Close");
+        CustomDialogFragment.newInstance(bundle).show(getActivity().getSupportFragmentManager(), CustomDialogFragment.TAG);
+    }
+
+    @OnClick({R.id.comments_iv, R.id.comment_count})
+    public void onclickComment(){
+
+        if(receivedPostModel.getComments() == null || receivedPostModel.getComments().size() == 0)
+            return;
+
+        tvNumOfComments.setText(receivedPostModel.getComments().size() + " Comments");
+
+        behavior = BottomSheetBehavior.from(bsComments);
+        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        break;
+
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+    }
+
+    @OnClick(R.id.ivDownArrow)
+    public void onClickDownArrow(){
+        behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+    }
+
 }
--- app/src/main/java/pros/app/com/pros/detail/presenter/DetailPresenter.java
+++ app/src/main/java/pros/app/com/pros/detail/presenter/DetailPresenter.java
@@ -1,4 +1,85 @@
 package pros.app.com.pros.detail.presenter;
 
-public class DetailPresenter {
+import pros.app.com.pros.R;
+import pros.app.com.pros.base.ApiEndPoints;
+import pros.app.com.pros.base.HttpServiceUtil;
+import pros.app.com.pros.base.HttpServiceView;
+import pros.app.com.pros.base.ProsConstants;
+import pros.app.com.pros.detail.view.DetailView;
+
+public class DetailPresenter implements HttpServiceView {
+
+    private DetailView detailView;
+
+    public DetailPresenter(DetailView detailView) {
+        this.detailView = detailView;
+    }
+
+    public void likePost(int id) {
+        new HttpServiceUtil(
+                this,
+                String.format(ApiEndPoints.like_post.getApi(), id),
+                ProsConstants.POST_METHOD,
+                null,
+                ApiEndPoints.like_post.getTag()
+        ).execute();
+
+    }
+
+    public void unlikePost(int id) {
+        new HttpServiceUtil(
+                this,
+                String.format(ApiEndPoints.unlike_post.getApi(), id),
+                ProsConstants.POST_METHOD,
+                null,
+                ApiEndPoints.unlike_post.getTag()
+        ).execute();
+    }
+
+    public void likeQuestion(int id) {
+        new HttpServiceUtil(
+                this,
+                String.format(ApiEndPoints.like_question.getApi(), id),
+                ProsConstants.POST_METHOD,
+                null,
+                ApiEndPoints.like_question.getTag()
+        ).execute();
+    }
+
+    public void unlikeQuestion(int id) {
+        new HttpServiceUtil(
+                this,
+                String.format(ApiEndPoints.unlike_question.getApi(), id),
+                ProsConstants.POST_METHOD,
+                null,
+                ApiEndPoints.unlike_question.getTag()
+        ).execute();
+    }
+
+    public void flagPost(int id) {
+        new HttpServiceUtil(
+                this,
+                String.format(ApiEndPoints.flag_post.getApi(), id),
+                ProsConstants.POST_METHOD,
+                null,
+                ApiEndPoints.flag_post.getTag()
+        ).execute();
+    }
+
+
+    @Override
+    public void response(String response, int tag) {
+        if (tag == ApiEndPoints.like_question.getTag() || tag == ApiEndPoints.like_post.getTag()) {
+            detailView.onLikeSuccess();
+        } else if (tag == ApiEndPoints.unlike_post.getTag() || tag == ApiEndPoints.unlike_question.getTag()) {
+            detailView.onUnLikeSuccess();
+        } else if (tag == ApiEndPoints.flag_post.getTag()) {
+            detailView.onflagPostSuccess();
+        }
+    }
+
+    @Override
+    public void onError(int tag) {
+        detailView.onFailure(R.string.internal_error);
+    }
 }
